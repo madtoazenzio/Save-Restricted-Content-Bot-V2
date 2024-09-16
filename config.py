01733be827275ba7335f5a4be18d61ea6efcdc1c
メ™Asʏɴ Eᴅɪᴛᴢ​ゞメ <89691212+madtoazenzio@users.noreply.github.com>
2024-09-16 15:28:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", "7236453"))
+API_HASH = getenv("API_HASH", "33010a70e94f80e55145980072cce969")
+BOT_TOKEN = getenv("BOT_TOKEN", "7166294138:AAHM32pCpz51w0T6UOhOYmNqhXMjeDzV4Ns")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 MONGO_DB = getenv("MONGO_DB", "")
 LOG_GROUP = getenv("LOG_GROUP", "")
